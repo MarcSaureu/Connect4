078e49ae57a7bf469e373c6faa951ab5bdedc9b4
Marc Saureu <marcsaureu@gmail.com>
2020-04-28 20:35:31 +0200

    Help Activity Done and layout created but no finished

---
+++ app/src/main/java/com/example/connect4/HelpActivity.java
@@ -0,0 +1,24 @@
+package com.example.connect4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        Button back = findViewById(R.id.BackButton);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/connect4/MainActivity.java
+++ app/src/main/java/com/example/connect4/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button help = findViewById(R.id.help_button);
+        Button start = findViewById(R.id.start_button);
+        Button exit = findViewById(R.id.exit_button);
+
+        help.setOnClickListener(this);
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.help_button:
+                Intent intent = new Intent (MainActivity.this,HelpActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.start_button:
+                Intent intent1 = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.exit_button:
+                break;
+        }
     }
 }
